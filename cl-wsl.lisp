ba7aa53c510e27b47ea7715818b4b771897ec99a
Dinesh Weerapurage <dweerapurage@mapanything.com>
2019-12-12 21:28:25 -0700

    read distance matrix to memory

--- cl-wsl.lisp
+++ cl-wsl.lisp
@@ -2,9 +2,13 @@
 
 (in-package #:cl-wsl)
 
+(defun generate-schedule()
+  )
+
 (defun build()
-  (let ((i 0) 
-	(country-table (make-hash-table :test #'equal)) 
+  (let ((i -1) 
+	(country-table (make-hash-table :test #'equal))
+	(distance-table nil)
 	(distance-list nil)) 
     (with-open-file (stream "dist.txt")
       (loop for line = (read-line stream nil :eof)
@@ -12,13 +16,19 @@
 	    do
 	    (let ((d (split-sequence #\Space line :remove-empty-subseqs t)))
 	      (push d distance-list)
-	      (cond 
-		((equal nil (gethash (first d) country-table))
-		 (setf (gethash (first d) country-table) i) 
-		 (format t "~a ~a ~r ~%" (first d) (second d) (parse-integer (third d)))
-		 (setf i (+ i 1)))
-		(t (format t "~a ~%" d)))))
-      (format t "~a ~%" distance-list)))) 
+	      (if (equal nil (gethash (first d) country-table))
+		 (setf (gethash (first d) country-table) (incf i)))
+	      (if (equal nil (gethash (second d) country-table))
+		(setf (gethash (second d) country-table) (incf i))))))
+    (let ((hcount 0))
+      (setf hcount (hash-table-count country-table))
+      (setf distance-table (make-array (list hcount hcount)))
+      (dolist (item distance-list)
+	(setf (aref distance-table 
+		    (gethash (first item) country-table) 
+		    (gethash (second item) country-table))
+	      (parse-integer (third item)))))
+      (format t "~a ~%" distance-table)))
 
 (defun main()
      (build))
