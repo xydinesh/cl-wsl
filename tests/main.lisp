18302320d719df9ce97c97aa254cc53d78854b54
Dinesh Weerapurage <xydinesh@gmail.com>
2020-01-05 20:16:53 -0700

    adding tests with byes

--- tests/main.lisp
+++ tests/main.lisp
@@ -7,18 +7,22 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-wsl)' in your Lisp.
 (deftest test-target-1
-         (testing "should (= 1 1) to be true"
-                  (ok (= 1 1))))
+  (testing "should (= 1 1) to be true"
+    (ok (= 1 1))))
 
 (deftest test-random-week
-         (testing "return a list with random numbers"
-                  (ok (= (length (random-week)) 12))))
+  (testing "return a list with random numbers"
+    (ok (= (length (random-week)) 12))))
 
 (deftest test-random-schedule
-         (testing "random schedule test"
-                  (ok (= (array-rank (random-schedule)) 2))
-                  (ok (equal (array-dimensions (random-schedule)) '(9 12)))))
+  (testing "random schedule test"
+    (ok (= (array-rank (random-schedule)) 2))
+    (ok (equal (array-dimensions (random-schedule)) '(9 12)))))
 
 (deftest test-byes-schedule
-         (testing "test for byes in the schedule"
-                  (fail "failed!")))
+  (let ((sched (random-schedule)) (i 0))
+    (testing "bye in random schedule"
+      (ok (> (aref sched 8 0) 100))
+      (ok (> (aref sched 8 1) 100))
+      (ok (> (aref sched 8 2) 100))
+      (ok (= (array-rank sched) 2)))))
