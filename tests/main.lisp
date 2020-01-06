cef6bb96a0626b63e1c0b9bc9d309e8e04cd9891
Dinesh Weerapurage <xydinesh@gmail.com>
2020-01-05 20:34:15 -0700

    adding a tests with fixed schedule so that easier for tests

--- tests/main.lisp
+++ tests/main.lisp
@@ -20,9 +20,27 @@
     (ok (equal (array-dimensions (random-schedule)) '(9 12)))))
 
 (deftest test-byes-schedule
-  (let ((sched (random-schedule)) (i 0))
+  (let ((sched (random-schedule)))
     (testing "bye in random schedule"
-      (ok (> (aref sched 8 0) 100))
-      (ok (> (aref sched 8 1) 100))
-      (ok (> (aref sched 8 2) 100))
-      (ok (= (array-rank sched) 2)))))
+      (dotimes (i 12)
+	(ok (>= (aref sched 8 i) 100))))))
+
+;;; We will be using this fixed schedule for generating more specific tests
+(defun test-sched()
+  #2A((8 7 -6 10 11 9 2 -1 0 -5 -3 -4)
+      (-11 9 -7 -5 -6 3 4 2 -10 -1 8 0)
+      (4 -11 -3 2 0 -8 -9 10 5 6 -7 1)
+      (-2 -3 0 1 -10 11 -8 -9 6 7 4 -5)
+      (4 -6 5 9 0 -2 1 -11 -10 -3 8 7)
+      (9 -10 -3 2 -11 7 8 -5 -6 0 1 4)
+      (-6 3 11 -1 7 -9 0 -4 10 5 -8 -2)
+      (1 0 9 6 -11 7 -3 -5 10 -2 -8 4)
+      (103 105 111 100 107 101 109 104 110 106 108 102)))
+
+(deftest test-sched
+  (let ((sched (test-sched)))
+    (testing "creating fixed schedule for tests"
+      (ok (= (array-rank sched) 2))
+      (ok (equal (array-dimensions sched) '(9 12)))
+      (dotimes (i 12)
+	(ok (>= (aref sched 8 i)))))))
